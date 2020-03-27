20f78dc4579fb00daaa81e24d4ed1802e5fd9fc5
Oisin Coveney <oisincoveney@gmail.com>
2020-03-27 13:05:40 -0700

    Modified seeder

--- api/seeders/20200326054237-demo-games-users.js
+++ api/seeders/20200326054237-demo-games-users.js
@@ -17,6 +17,27 @@ module.exports = {
     let p = models.sequelize.sync()
 
     await p.then(() => {
+      queryInterface.bulkInsert('Games', [
+        {
+          gameID: "abc1234",
+          nextCard: 2,
+          cardOrder: "[A-D, A-S, A-H, A-C]",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          RuleSetId: 1,
+        },
+        {
+          gameID: "def6789",
+          nextCard: 5,
+          cardOrder: "[A-D, A-S, A-H, A-C]",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          RuleSetId: 1,
+        },
+      ])
+    })
+
+    await p.then(() => {
       queryInterface.bulkInsert('Users', [
         {
           firstName: "John",
@@ -43,53 +64,6 @@ module.exports = {
       ])
     })
 
-    await p.then(() => {
-      queryInterface.bulkInsert('Games', [
-        {
-          firstName: "John",
-          lastName: "Doe",
-          email: "johndoe@gmail.com",
-          wins: "420",
-          losses: "0",
-          gamesPlayed: "69",
-          favoriteDrink: "Corona",
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          firstName: "Dim",
-          lastName: "Sum",
-          email: "dimsum@gmail.com",
-          wins: "0",
-          losses: "420",
-          gamesPlayed: "69",
-          favoriteDrink: "Bud Light Lime",
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ])
-    })
-
-    await p.then(() => {
-      queryInterface.bulkInsert('Games', [
-        {
-          gameID: "abc1234",
-          nextCard: 2,
-          cardOrder: "[A-D, A-S, A-H, A-C]",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          RuleSetId: 1,
-        },
-        {
-          gameID: "def6789",
-          nextCard: 5,
-          cardOrder: "[A-D, A-S, A-H, A-C]",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          RuleSetId: 1,
-        },
-      ])
-    })
     await p.then(() => {
       queryInterface.bulkInsert('GameUser', [
         {
